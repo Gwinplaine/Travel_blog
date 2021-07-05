f268663df15ffd7d11bafe680775fc35802a1240
Denis Vashkevich <3488927@gmail.com>
2021-07-05 22:05:26 +0300

    commit final before defence

--- notes/models.py
+++ notes/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 # создание модели заметки Notesentry с указанием названия заметки, текста заметки, даты добавления заметки,
 # автора заметки
 class Notesentry(models.Model):
-    '''информация, изученная по теме'''
     notestitle = models.CharField(max_length=50)
     notestext = models.TextField()
     notesdate_added = models.DateTimeField(auto_now_add=True)
@@ -13,7 +12,6 @@ class Notesentry(models.Model):
 
     # возвращает представление текста как 'текст'+'...', если длина заметки составляет более 50 знаков
     def __str__(self):
-        '''возвращает строковое представление модели'''
         if len(self.notestext) > 50:
             return self.notestext[:50] + '...'
         else:
