72d39d257388cea19811e9f106316168cd27a9b5
Denis Vashkevich <3488927@gmail.com>
2022-04-18 22:09:04 +0300

    refresh

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+a = int(input())
+if a == 1000:
+    print('someshit')
+elif a == 1500:
+    print("another somesh#t")
+else:
+    print("f#ck off /|yPaK")
